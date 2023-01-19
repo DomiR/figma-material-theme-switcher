ad8fa5da370160a3d5bc19f5ec4c49c1338b249a
Dominique <domi.github@gmail.com>
2023-01-19 09:42:03 +0100

    feat(plugin): remove loading and clean up plugin code

--- code.ts
+++ code.ts
@@ -1,28 +1,16 @@
-// This file holds the main code for the plugin. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
 async function main() {
   if (figma.command === 'openSwitcher') {
-    // This shows the HTML page in "ui.html".
-    console.log('running 1.0.1')
+    await TeamColorsManager.loadTeamStyles()
+    figma.notify('Loaded team styles', {timeout: 2000})
     figma.showUI(__html__, {height: 70, width: 70})
   } else if (figma.command === 'saveFromTeamLibrary') {
     await TeamColorsManager.saveTeamStyleKeysToStorage()
-    figma.notify('Saved team colors to storage', {timeout: 2000})
-    figma.closePlugin();
-  } else if (figma.command === 'loadFromTeamLibrary') {
-    await TeamColorsManager.loadTeamStyles()
-    figma.notify('Loaded team colors to storage', {timeout: 2000})
+    figma.notify('Saved team styles', {timeout: 2000})
     figma.closePlugin();
   }
 }
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
 figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
   if (msg.type === 'dark') {
     replaceAllStyles('dark')
   } else if (msg.type === 'light') {
@@ -32,10 +20,6 @@ figma.ui.onmessage = msg => {
   } else if (msg.type === 'load') {
     TeamColorsManager.loadTeamStyles()
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  //figma.closePlugin();
 };
 
 async function replaceAllStyles(mode: 'light' | 'dark') {
@@ -56,14 +40,9 @@ async function replaceAllStyles(mode: 'light' | 'dark') {
 
 function replaceNodes(mode: 'light' | 'dark', styleManager: StyleManager, nodes: Array<any>): void {
   for (const node of nodes) {
-    console.log('processing node', node.name, node.type, node)
     const backgroundStyleName = styleManager.getStyleNameById(node.backgroundStyleId)
     const fillStyleName = styleManager.getStyleNameById(node.fillStyleId)
     const strokeStyleName = styleManager.getStyleNameById(node.strokeStyleId)
-    console.log('fillStyleName', fillStyleName)
-    console.log('strokeStyleName', strokeStyleName)
-    console.log('backgroundStyleName', backgroundStyleName)
-    console.log('-----')
     if (fillStyleName != null) {
       const replacedColorStyleName = Replacer.replace(fillStyleName, mode)
       const replacedFillStyleId = styleManager.getStyleIdByName(replacedColorStyleName)
@@ -113,10 +92,8 @@ class TeamColorsManager {
       return []
     }
 
-    console.log('start loading: ', teamColorKeys.length, ' keys')
     const teamStylesResults = await Promise.all(teamColorKeys.map((k: string) => figma.importStyleByKeyAsync(k).catch(_e => null)))
     const teamStyles = teamStylesResults.filter(s => s != null)
-    console.log('loaded team: ', teamStyles)
     const styles = teamStyles.map(a => ({id: a.id, name: a.name}))
     await figma.clientStorage.setAsync(this.styles, JSON.stringify(styles))
     return teamStyles
@@ -128,7 +105,6 @@ class TeamColorsManager {
       console.log("The team colors were not found. Please run 'save' on the styles page before run any replace commands.")
       return []
     }
-    console.log('loaded team: ', teamColors)
     return JSON.parse(teamColors)
   }
 }
